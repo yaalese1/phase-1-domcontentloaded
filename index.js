6cc123c9cee698a0911eea3d9eaccfb53f77e63f
Yetunde Alese <yaalese1@gmail.com>
2022-11-08 16:02:03 -0500

    commit tree b66eaf8dd4fc175e60513643b86c281501ad7bdc

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+
+    function changeWebText (){
+        document.getElementById('text').textContent = 'This is really cool!'
+       
+       }
+       changeWebText()
+  });
+
+
+
